314745bcf0972e8fc57e25f0e343de060cbead73
Ruben Alba Gonzalez <39350323+Ruben-Alba-Gonzalez@users.noreply.github.com>
2026-01-30 19:52:59 +0000

    Añadida funcionalidad del semaforo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,24 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [active,setActive] = useState();
+
+	return (
+		<div className="min-vh-100 d-flex">
+			<div className="d-flex bg-dark rounded m-auto flex-column p-3">
+				<div className={active=="rounded-circle m-1 red"? active+" selected-red" : "rounded-circle m-1 red"} style={{width: "75px",height: "75px"}} onClick={()=>{
+					setActive("rounded-circle m-1 red")
+				}}></div>
+				<div className={active=="rounded-circle m-1 yellow"? active+" selected-yellow" : "rounded-circle m-1 yellow"} style={{ width: "75px", height: "75px" }} onClick={()=>{
+					setActive("rounded-circle m-1 yellow")
+				}}></div>
+				<div className={active=="rounded-circle m-1 green"? active+" selected-green" : "rounded-circle m-1 green"} style={{ width: "75px", height: "75px" }} onClick={()=>{
+					setActive("rounded-circle m-1 green")
+				}}></div>
+			</div>
 		</div>
 	);
 };
