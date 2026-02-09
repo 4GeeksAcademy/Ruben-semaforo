7200864ff946446a77e3ce269089052bcb79a4b5
Ruben Alba Gonzalez <39350323+Ruben-Alba-Gonzalez@users.noreply.github.com>
2026-02-09 20:37:04 +0000

    Añadidas algunas mejoras

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,23 +1,48 @@
 import React from "react";
 import { useState } from "react";
+import Light from "./Light";
 
 //create your first component
 const Home = () => {
 
-	const [active,setActive] = useState();
+	const [active, setActive] = useState("");
+	
+	const activeFunction = (color) => {
+		if(active.includes(color)){
+			setActive("");
+			return
+		}
+		setActive(`rounded-cicler light m-1 ${color}`)
+	}
 
 	return (
 		<div className="min-vh-100 d-flex">
 			<div className="d-flex bg-dark rounded m-auto flex-column p-3">
-				<div className={active=="rounded-circle m-1 red"? active+" selected-red" : "rounded-circle m-1 red"} style={{width: "75px",height: "75px"}} onClick={()=>{
-					setActive("rounded-circle m-1 red")
-				}}></div>
-				<div className={active=="rounded-circle m-1 yellow"? active+" selected-yellow" : "rounded-circle m-1 yellow"} style={{ width: "75px", height: "75px" }} onClick={()=>{
-					setActive("rounded-circle m-1 yellow")
-				}}></div>
-				<div className={active=="rounded-circle m-1 green"? active+" selected-green" : "rounded-circle m-1 green"} style={{ width: "75px", height: "75px" }} onClick={()=>{
-					setActive("rounded-circle m-1 green")
-				}}></div>
+
+				<Light clases={active.includes("red") ? active + " selected-red" : "rounded-circle light m-1 red"} utility={() => {
+					if (active.includes("red")) {
+						setActive("")
+						return
+					}
+					setActive("rounded-circle light m-1 red")
+				}} />
+				
+				<Light clases={active.includes("yellow") ? active + " selected-yellow" : "rounded-circle light m-1 yellow"} utility={() => {
+					if (active.includes("yellow")) {
+						setActive("")
+						return
+					}
+					setActive("rounded-circle light m-1 yellow")
+				}} />
+
+				<Light clases={active.includes("green") ? active + " selected-green" : "rounded-circle light m-1 green"} utility={() => {
+					if (active.includes("green")) {
+						setActive("")
+						return
+					}
+					setActive("rounded-circle light m-1 green")
+				}} />
+
 			</div>
 		</div>
 	);
---
+++ src/js/components/Light.jsx
@@ -0,0 +1,9 @@
+function Light({clases, utility}){
+    return(
+        <div className={clases} onClick={utility}>
+
+        </div>
+    )
+};
+
+export default Light;
